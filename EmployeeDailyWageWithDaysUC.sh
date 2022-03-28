5fd18e8bb271af12d01ee0c3efb6b9cb5372addb
jhanvi006 <jhanvikankhara06@gmail.com>
2022-03-28 12:17:41 +0530

    add daily wage calculation along with total days and total wage use case.

---
+++ EmployeeDailyWageWithDaysUC.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+function calculateWorkingHr(){
+	case $1 in
+			0)
+				workingHr=0;
+				;;
+			1)
+				workingHr=8;
+				;;
+			2)
+				workingHr=4;
+				;;
+	esac;
+	echo $workingHr;
+}
+
+perHrSalary=20;
+totalSalary=0;
+totalWorkingHr=0;
+day=1;
+
+declare -A dailyWage
+while [[ $day -le 20 && $totalWorkingHr -lt 100 ]]
+do
+	wHr=$(calculateWorkingHr $((RANDOM%3)) );
+	totalWorkingHr=$(($totalWorkingHr+$wHr));
+	dailyWage[$day]=$totalWorkingHr;
+	if [ $totalWorkingHr -gt 100 ]
+	then
+		totalWorkingHr=$(($totalWorkingHr-$wHr));
+		dailyWage[$day]=$totalWorkingHr;
+		break;
+	fi
+	salary=$(($perHrSalary*$wHr));
+	totalSalary=$(($totalSalary+$salary));
+	((day++));
+done
+
+echo "Employee has earned $totalSalary $ in a month (Total working hrs: $totalWorkingHr)";
+echo "Total working hours: "${!dailyWage[@]};
+echo "Daily wage: "${dailyWage[@]};
+echo "DailyWage/hr:"${perHrSalary[@]};
+echo "Total wage:"${totalSalary[@]};
+echo "Total day:"${day[@]};
