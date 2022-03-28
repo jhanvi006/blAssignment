c83fbdefbf6a251f74ea5677c29040a9ec27758f
jhanvi006 <jhanvikankhara06@gmail.com>
2022-03-28 12:10:30 +0530

    add daily wage calculation use case.

--- EmployeeDailyWageUC.sh
+++ EmployeeDailyWageUC.sh
@@ -1,15 +1,42 @@
 #!/bin/bash -x
 
-checkPresence=$((RANDOM%2));
-wagePrHour=20;
+function calculateWorkingHr(){
+	case $1 in
+			0)
+				workingHr=0;
+				;;
+			1)
+				workingHr=8;
+				;;
+			2)
+				workingHr=4;
+				;;
+	esac;
+	echo $workingHr;
+}
 
-if (( $checkPresence==1 ))
-then
-	#employee is present
-	workingHour=8;
-else
-	#employee is absent
-	workingHour=0;
-fi
-salary=$(($workingHour*$wagePrHour));
-echo "$salary $ for working $workingHour hrs.";
+perHrSalary=20;
+totalSalary=0;
+totalWorkingHr=0;
+day=1;
+
+declare -A dailyWage
+while [[ $day -le 20 && $totalWorkingHr -lt 100 ]]
+do
+	wHr=$(calculateWorkingHr $((RANDOM%3)) );
+	totalWorkingHr=$(($totalWorkingHr+$wHr));
+	dailyWage[$day]=$totalWorkingHr;
+	if [ $totalWorkingHr -gt 100 ]
+	then
+		totalWorkingHr=$(($totalWorkingHr-$wHr));
+		dailyWage[$day]=$totalWorkingHr;
+		break;
+	fi
+	salary=$(($perHrSalary*$wHr));
+	totalSalary=$(($totalSalary+$salary));
+	((day++));
+done
+
+echo "Employee has earned $totalSalary $ in a month (Total working hrs: $totalWorkingHr)";
+echo "Total working hours: "${!dailyWage[@]};
+echo "Daily wage: "${dailyWage[@]};
