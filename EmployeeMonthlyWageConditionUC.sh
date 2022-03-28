aa258a205a170b8dcec7bc18b3e9d113c34e9563
jhanvi006 <jhanvikankhara06@gmail.com>
2022-03-28 11:41:21 +0530

    add monthly wage calculation with condition use case.

---
+++ EmployeeMonthlyWageConditionUC.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+perHrSalary=20;
+totalSalary=0;
+totalWorkingHr=0;
+day=1;
+
+checkWorkingTime=$((RANDOM%3));
+case $checkWorkingTime in
+	0)
+   #employee is absent
+   workingHour=0;
+	;;
+	1)
+	#employee is present and working part time
+	workingHour=8;
+	;;
+	2) 
+	#employee is present and working full time
+	workingHour=16;
+	;;
+esac
+
+while [[ $day -le 20 && $totalWorkingHr -lt 100 ]]
+do
+	totalWorkingHr=$(($totalWorkingHr+$workingHour));
+	if [ $totalWorkingHr -gt 100 ]
+	then
+		totalWorkingHr=$(($totalWorkingHr-$workingHour));
+		break;
+	fi
+	salary=$(($perHrSalary*$workingHour));
+	totalSalary=$(($totalSalary+$salary));
+	((day++));
+done
+
+echo "Employee has earned $totalSalary $ in a month (Total working hrs: $totalWorkingHr)";
